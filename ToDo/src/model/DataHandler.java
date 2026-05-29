5c58d1cbe6259d64206fbdecd010b66d659ca297
Simon Kendel <simon.kendel@htl-wels.at>
2026-05-29 18:03:11 +0200

    v0.2 GUI falsch

---
+++ ToDo/src/model/DataHandler.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.LinkedList;
+
+public class DataHandler {
+    private LinkedList<String> listEingelesen = new LinkedList<>();
+    private LinkedList<String> listBerechnet = new LinkedList<>();
+
+
+    public String printList() {
+        String str = "";
+        for (String x : listEingelesen) {
+            str += x;
+            str += "\n";
+        }
+        return str;
+    }
+
+    public LinkedList<String> getListBerechnet() {
+        return listBerechnet;
+    }
+
+    public void setListBerechnet(LinkedList<String> listBerechnet) {
+        this.listBerechnet = listBerechnet;
+    }
+
+    public LinkedList<String> getListEingelesen() {
+        return listEingelesen;
+    }
+
+    public void setListEingelesen(LinkedList<String> listEingelesen) {
+        this.listEingelesen = listEingelesen;
+    }
+}
---
+++ ToDo/src/viewctrl/Controller_login.java
@@ -0,0 +1,66 @@
+package viewctrl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import model.DataHandler;
+import model.FileHandler;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller_login implements Initializable {
+    String datei_users = "D:\\SIMON\\3BHIT\\SEW\\Üben\\4. PLF\\Ueben_1\\src\\datei.txt";
+
+    DataHandler dataHandler;
+    FileHandler fileHandler;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField tfUsername;
+
+    @FXML
+    void btnLoginPressed(ActionEvent event) {
+
+    }
+
+//    @FXML
+//    void btnLoadPressed(ActionEvent event) {
+//        String str;
+//        dataHandler.setListEingelesen(fileHandler.dateiEinlesen(datei_users));
+//        str = dataHandler.getListEingelesen().toString();
+//        textArea.setText(dataHandler.printList());
+//    }
+//
+//    @FXML
+//    void btnCalcPressed(ActionEvent event) throws IOException {
+//        main.Main.loadScene("/viewctrl/logged_in_user.fxml");
+//
+//    }
+
+
+//    @FXML
+//    void btnLoginPressed(ActionEvent event) {
+//
+////        fileHandler.dateiSchreiben(dateiZiel, dataHandler.getListBerechnet());
+//    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        String hallo = "aölkjsdf";
+        System.out.println(hallo.matches("[n]"));
+        dataHandler = new DataHandler();
+        fileHandler = new FileHandler();
+    }
+}
